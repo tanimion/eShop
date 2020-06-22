15bf58403bf736339f0c75ac7a92406b9de259f3
Kowser Mahmud Tanim <pay2tanim@gmail.com>
2020-06-23 00:49:41 +0600

    Project Upload

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lenovo\desktop\my_django_project_3\my_ecom_project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
